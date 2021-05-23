bf491ac603b81739f9c4bcfaf833c77ec5feb5f8
MandeepDalavi <mandeepdalavi@gmail.com>
2021-05-24 00:12:26 +0530

    Navigation Menu Controls

--- js/main.js
+++ js/main.js
@@ -1,3 +1,72 @@
+/* navigation menu */
+
+(() => {
+
+    const hamburgerBtn = document.querySelector(".hamburger-btn"),
+    navMenu = document.querySelector(".nav-menu"),
+    closeNavBtn = navMenu.querySelector(".close-nav-menu");
+
+    hamburgerBtn.addEventListener("click", showNavMenu);
+    closeNavBtn.addEventListener("click", hideNavMenu);
+
+    function showNavMenu() {
+        navMenu.classList.toggle("open");
+        bodyScrollingToggle();
+    }
+    function hideNavMenu() {
+        navMenu.classList.toggle("open");
+        fadeOutEffect();
+        bodyScrollingToggle();
+    }
+    function fadeOutEffect() {
+        document.querySelector(".fade-out-effect").classList.add("active");
+        setTimeout(() => {
+            document.querySelector(".fade-out-effect").classList.remove("active");
+        },300)
+    }
+    // attach an event handler to document
+    document.addEventListener("click", (event) => {
+        if(event.target.classList.contains('link-item')){
+            // even.target.hash should have a value before overriding default behavior
+            if(event.target.hash !== ""){
+                // prevent default snchor click behavior
+                event.preventDefault();
+                const hash = event.target.hash;
+                console.log(hash);
+                // deactivate existing active 'section'
+                document.querySelector(".section.active").classList.add("hide");
+                document.querySelector(".section.active").classList.remove("active");
+                // activate new 'section'
+                document.querySelector(hash).classList.add("active");
+                document.querySelector(hash).classList.remove("hide");
+                // deactivate existing active navigation menu 'link-item'
+                navMenu.querySelector(".active").classList.add("outer-shadow", "hover-in-shadow");
+                navMenu.querySelector(".active").classList.remove("active", "inner-shadow");
+                // if clicked 'link-item is contained within the navigation menu'
+                if(navMenu.classList.contains("open")){
+                    // activate new nevigation menu 'link-item'
+                    event.target.classList.add("active", "inner-shadow");
+                    event.target.classList.remove("outer-shadow", "hover-in-shadow");
+                    // hide navigation menu
+                    hideNavMenu();
+                }
+                else{
+                    let navItems = navMenu.querySelectorAll(".link-item");
+                    navItems.forEach((item) => {
+                        if(hash === item.hash){
+                            // activate new nevigation menu 'link-item'
+                            item.classList.add("active", "inner-shadow");
+                            item.classList.remove("outer-shadow","hover-in-shadow");
+                        }
+                    })
+                    fadeOutEffect();
+                }
+            }
+        }
+    })
+
+})();
+
 /* about section tabs */
 
 (() =>{
