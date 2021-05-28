7b62ad0ca2ec16ce6bf66529f70c740ccf7f8826
MandeepDalavi <mandeepdalavi@gmail.com>
2021-05-28 21:37:31 +0530

    Solved some Theme and Dark Mode issues

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -5,7 +5,7 @@ styleSwitcherToggler.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 })
 
-// hide style-switcher on scroll
+/* hide style-switcher on scroll */
 window.addEventListener("scroll", () => {
     if(document.querySelector(".style-switcher").classList.contains("open")){
         document.querySelector(".style-switcher").classList.remove("open")
@@ -15,30 +15,61 @@ window.addEventListener("scroll", () => {
 /* theme colors */
 const alternateStyles = document.querySelectorAll(".alternate-style");
 function setActiveStyle(color) {
+    localStorage.setItem("color", color);
+    changeColor();
+}
+
+function changeColor() {
     alternateStyles.forEach((style) => {
-        if(color === style.getAttribute("title")){
+        if(localStorage.getItem("color") === style.getAttribute("title")){
             style.removeAttribute("disabled");
         }
         else{
-            style.setAttribute("disabled", "true");
+            style.setAttribute("disabled","true");
         }
     })
+}
+
+// checking if 'color' key exists
+if(localStorage.getItem("color") !== null) {
+    changeColor();
 }
 
 /* theme light & dark mode */
 const dayNight = document.querySelector(".day-night");
 
 dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun");
-    dayNight.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("dark");
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("theme", "dark");
+    }
+    else{
+        localStorage.setItem("theme", "light");
+    }
+    updateIcon();
 })
 
-window.addEventListener("load", () => {
+function themeMode() {
+    // checking if 'theme' key exists
+    if(localStorage.getItem("theme") !== null) {
+        if(localStorage.getItem("theme") === "light"){
+            document.body.classList.remove("dark");
+        }
+        else{
+            document.body.classList.add("dark");
+        }
+    }
+    updateIcon();
+}
+themeMode();
+
+function updateIcon() {
     if(document.body.classList.contains("dark")){
+        dayNight.querySelector("i").classList.remove("fa-moon");
         dayNight.querySelector("i").classList.add("fa-sun");
     }
     else{
+        dayNight.querySelector("i").classList.remove("fa-sun");
         dayNight.querySelector("i").classList.add("fa-moon");
     }
-})
+}
